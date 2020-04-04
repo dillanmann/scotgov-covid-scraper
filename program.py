2ebec8becc538d2a81925ab234b88f1c37b82247
Dillanm <dillan.mann@gmail.com>
2020-04-05 00:14:37 +0100

    Update to use requests for current data rather than selenium Remove args as no longer needed

--- program.py
+++ program.py
@@ -2,39 +2,41 @@ import requests
 import os
 import sys
 from selenium import webdriver
-from selenium.webdriver.common.keys import Keys  
-from selenium.webdriver.chrome.options import Options  
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.chrome.options import Options
 from src.scotgov_covid_scraper import ScotgovCovidScraper
 from src.data_uploader import DataUploader
 from src.data_set import Dataset
 
-if __name__ == "__main__":
-
-    args = sys.argv
-    if len(args) < 2:
-        raise Exception(
-            ('No URLS specified to parse. Provide urls by calling the program like: \n'
-                'python program.py "<url>;<url>"'))
-
-    urls = args[1].split(';')
+def get_page_selenium(url):
     chrome_opts = Options()
-    chrome_opts.add_argument("--headless")  
+    chrome_opts.add_argument("--headless")
     chrome_opts.binary_location = 'C:\\Program Files (x86)\\Google\\Chrome\\Application\\chrome.exe'
 
     driver = webdriver.Chrome(chrome_options=chrome_opts)
+    driver.get(url)
+    return driver.page_source
+
+
+def get_page_requests(url):
+    return requests.get(url).text
+
+
+if __name__ == "__main__":
+
+    urls = ['https://www.gov.scot/coronavirus-covid-19/']
+
     for url in urls:
-        driver.get(url)
+        page_source = get_page_requests(url)
 
-        if len(driver.find_elements_by_id('overview')) == 0:
-            raise Exception("Failed to find root data element")
-
-        covid_scraper = ScotgovCovidScraper(driver.page_source)
+        covid_scraper = ScotgovCovidScraper(page_source)
 
         date = None
         try:
             date = covid_scraper.get_date()
         except:
-            date_in = input("no date found, enter date for URL {} in format DD MONTH YYYY".format(url))
+            date_in = input(
+                "no date found, enter date for URL {} in format DD MONTH YYYY".format(url))
             date = covid_scraper.parse_date(date_in)
 
         print(date)
@@ -54,7 +56,8 @@ if __name__ == "__main__":
         cases_by_healthboard = covid_scraper.get_health_board_cases()
         print(cases_by_healthboard)
 
-        dataset = Dataset(date, total_tests, positive_cases, negative_cases, total_deaths, cases_by_healthboard)
+        dataset = Dataset(date, total_tests, positive_cases,
+                          negative_cases, total_deaths, cases_by_healthboard)
 
         with DataUploader() as uploader:
             uploader.upload_data(dataset)
